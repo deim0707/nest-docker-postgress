58e23d37e08dba411b58af8442bc67e2641af970
Dmitry <dm3iy.p@gmail.com>
2021-07-24 16:56:29 +0300

    install packages for DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+// не забываем установить локально на машине postgres. к которому в т.ч. идёт pgAdmin4
+const sequelizeModule = SequelizeModule.forRoot({
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432, //порт по умолчанию у Постграсса
+  username: 'postgres',
+  password: 'root123',
+  database: 'nest-course',
+  models: [],
+  autoLoadModels: true, // с этим флагом севелайз создаёт таблицы в БД на основании моделей, что мы здесь создаём
+});
 
 // декораторы добавляет классу или функции новый функционал
 @Module(
@@ -8,6 +21,7 @@ import { AppService } from './app.service';
   {
     controllers: [AppController],
     providers: [AppService],
+    imports: [sequelizeModule],
   },
 )
 export class AppModule {}
